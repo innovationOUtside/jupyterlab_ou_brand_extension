688863cb79557920b1950a9a9b0331ccedcdac39
Tony Hirst <tony.hirst@gmail.com>
2022-04-29 15:12:29 +0100

    First attempt

--- style/index.js
+++ style/index.js
@@ -1,1 +1,16 @@
 import './base.css';
+
+// Via: https://discourse.jupyter.org/t/changing-favicon-with-notebook-extension/2721
+
+let head = document.head || document.getElementsByTagName('head')[0];
+
+let link = document.createElement('link')
+let oldLink = document.getElementsByClassName('favicon');
+link.rel = 'icon';
+link.type = 'image/x-icon';
+link.href = 'https://www.open.ac.uk/oudigital/v4/external/img/favicons/favicon.png';
+if (oldLink) {
+    link.classList = oldLink[0].classList;
+    head.removeChild(oldLink[0]);
+}
+head.appendChild(link);
